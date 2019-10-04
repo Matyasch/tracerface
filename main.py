cee349a21af5f627e482c84afb974df3891fe4e3
MatyiDev <schubertmatyas@gmail.com>
2019-10-04 23:01:38 +0200

    Better spacing in code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+
 from model import Model
 from persistence import Persistence
 from viewmodel import ViewModel
 from view import View
-
 
 def main():
     persistence = Persistence()
@@ -11,6 +11,7 @@ def main():
     view_model = ViewModel(model)
     view = View(view_model)
     view.app.run_server(debug=True)
+
 
 if __name__ == '__main__':
     main()
