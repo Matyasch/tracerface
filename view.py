db70a073556cc378e82fc3f78b92445e5763b28e
MatyiDev <schubertmatyas@gmail.com>
2019-10-04 23:02:20 +0200

    Change to more efficient import

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-import dash
+from dash import Dash
 
 from callback_manager import CallbackManager
 from layout import Layout
@@ -8,7 +8,7 @@ class View:
         self.view_model=view_model
         self.layout = Layout(self.view_model)
 
-        self.app = dash.Dash(__name__)
+        self.app = Dash(__name__)
         self.app.layout = self.layout.app_layout()
 
         self.callback_manager = CallbackManager(self.app, self.view_model)
