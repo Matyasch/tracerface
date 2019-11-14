ddcc06ff8ae84f2a2dd11c8428d6a413aba8b6f6
MatyiDev <schubertmatyas@gmail.com>
2019-11-14 23:09:30 +0100

    Give title and disable debugging

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ class App:
         self._configuration = Configuration()
         self._view_model=ViewModel(self._configuration)
         self._layout = Layout(self._view_model)
-
         self._app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+        self._app.title = 'Visual Trace'
         self._app.layout = self._layout.layout()
 
         self.callback_manager = CallbackManager(self._app, self._view_model, self._layout)
         self.callback_manager.setup_callbacks()
 
     def start(self):
-        self._app.run_server(debug=True)
+        self._app.run_server(debug=False)
