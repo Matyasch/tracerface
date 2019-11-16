b5d7b4fe88d146373e302378c260257d71bb9c7e
MatyiDev <schubertmatyas@gmail.com>
2019-11-16 10:02:20 +0100

    Make alerts object-oriented

--- view/alerts.py
+++ view/alerts.py
@@ -3,56 +3,62 @@ from dash_html_components import P
 
 
 #base alerts
-def base_success_alert(message):
-    return Alert(message, color='success', duration=4000, dismissable=True)
+class BaseAlert(Alert):
+    def __init__(self, message, color):
+        super().__init__(message, color=color, duration=4000, dismissable=True)
 
-def base_error_alert(message):
-    return Alert(message, color='danger', duration=4000, dismissable=True)
+class SuccessAlert(BaseAlert):
+    def __init__(self, message):
+        super().__init__(message=message, color='success')
+
+class ErrorAlert(BaseAlert):
+    def __init__(self, message):
+        super().__init__(message=message, color='danger')
 
 #application add alerts
 def app_already_added_alert():
-    return base_error_alert('Application already added')
+    return ErrorAlert('Application already added')
 
 def add_app_success_alert(app):
     return Alert('{} was added successfully'.format(app), color='success', duration=4000, dismissable=True)
 
 def empty_app_name_alert():
-    return base_error_alert('Please provide an application name')
+    return ErrorAlert('Please provide an application name')
 
 #manage app alerts
 def no_app_selected_alert():
-    return base_error_alert('Please select an application first')
+    return ErrorAlert('Please select an application first')
 
 #add function alerts
 def empty_function_name_alert():
-    return base_error_alert('Please provide a function name')
+    return ErrorAlert('Please provide a function name')
 
 def function_already_added_alert():
-    return base_error_alert('Function already added to this application')
+    return ErrorAlert('Function already added to this application')
 
 def func_add_success_alert():
-    return base_success_alert('Function successfully added')
+    return SuccessAlert('Function successfully added')
 
 #manage function alert
 def no_func_selected_alert():
-    return base_error_alert('Please select a function first')
+    return ErrorAlert('Please select a function first')
 
 #add parameter alerts
 def no_param_type_alert():
-    return base_error_alert('Please provide the type of the parameter')
+    return ErrorAlert('Please provide the type of the parameter')
 
 def no_param_index_alert():
-    return base_error_alert('Please provide the position of the parameter')
+    return ErrorAlert('Please provide the position of the parameter')
 
 def param_already_added_alert():
-    return base_error_alert('Parameter already added to this position')
+    return ErrorAlert('Parameter already added to this position')
 
 def param_add_success_alert():
-    return base_success_alert('Parameter successfully added')
+    return SuccessAlert('Parameter successfully added')
 
 #manage parameter alerts:
 def no_param_selected_alert():
-    return base_error_alert('Please select a parameter first')
+    return ErrorAlert('Please select a parameter first')
 
 #trace alerts
 def trace_error_alert(error):
@@ -60,14 +66,14 @@ def trace_error_alert(error):
 
 #load output alerts
 def load_output_success_alert():
-    return base_success_alert('Output loaded')
+    return SuccessAlert('Output loaded')
 
 def output_empty_alert():
-    return base_error_alert('Please provide the output of a BCC trace run')
+    return ErrorAlert('Please provide the output of a BCC trace run')
 
 #configuration save alerts
 def empty_command_config_alert():
-    return base_error_alert('Please provide a command')
+    return ErrorAlert('Please provide a command')
 
 def save_config_success_alert():
-    return base_success_alert('Configuration saved!')
+    return SuccessAlert('Configuration saved!')
