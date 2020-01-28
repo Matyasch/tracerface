a940438466126b8aeae78ef6bdfd3e01203cdf2c
MatyiDev <schubertmatyas@gmail.com>
2020-01-28 17:51:24 +0100

    Add comments to base model

--- model/base.py
+++ model/base.py
@@ -1,45 +1,59 @@
+'''
+Parent class of the two types of models used.
+Provides basic functionality and persistence handling.
+'''
 from persistence.persistence import Persistence
 
 
-# Manages logic and persistence
 class BaseModel:
     def __init__(self, configuration):
         self._persistence = Persistence()
         self._configuration = configuration
 
+    # returns all nodes in a list
     def get_nodes(self):
         return self._persistence.get_nodes()
 
+    # returns all edges in a list
     def get_edges(self):
         return self._persistence.get_edges()
 
+    # returns the lower bound for coloring elements yellow
     def yellow_count(self):
         return self._persistence.get_yellow()
 
+    # returns the lower bound for coloring elements red
     def red_count(self):
         return self._persistence.get_red()
 
+    # returns the maximum number of calls among nodes
     def max_count(self):
         call_counts = [node['call_count'] for node in self._persistence.get_nodes().values()]
         if call_counts:
             return max(call_counts)
         return 0
 
+    # set new values for the coloring bounds
     def set_range(self, yellow, red):
         self._persistence.update_colors(yellow, red)
 
+    # set command for bcc trace
     def save_app_config(self, bcc_command):
         self._configuration.update_command(bcc_command)
 
+    # set animation state and spacing between nodes
     def save_layout_config(self, animate, spacing):
         self._configuration.update_layout(animate, spacing)
 
+    # get spacing between nodes
     def get_spacing_config(self):
         return self._configuration.get_spacing()
 
+    # get wether animation is turned on or not
     def get_animate_config(self):
         return self._configuration.get_animate()
 
+    # initialize color boundaries to default values based on maximum count
     def init_colors(self):
         yellow = round(self.max_count()/3)
         red = round(self.max_count()*2/3)
