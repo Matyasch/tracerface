f07f4f629ac05d36775062673b598d8d7b785ad5
MatyiDev <schubertmatyas@gmail.com>
2020-01-29 17:28:19 +0100

    Simplify configure tab

--- view/configure_tab.py
+++ view/configure_tab.py
@@ -9,21 +9,27 @@ class ConfigureTab(dbc.Tab):
         super().__init__(label='Configure', tab_id='configure-tab', id='configure-tab', children=self._content())
 
     def _content(self):
-        return Div(children=[
-            dbc.FormGroup(children=[
-                dbc.Label('Command for bcc: ', width=5),
-                dbc.Col(dbc.Input(
-                    id='bcc-command',
-                    type='text',
-                    value='trace-bpfcc',
-                    placeholder='command'))],
-                row=True,
-                style=button_style()),
+        return dbc.FormGroup(
+            children=[
+                self._command_settings_group(),
+                self._graph_settings_group(),
+                self._save_changes_group()
+            ],
+            style=tab_style())
+
+    def _command_settings_group(self):
+        return dbc.FormGroup([
+            dbc.Label('Command for bcc'),
+            dbc.Input(id='bcc-command', type='text', value='trace-bpfcc', placeholder='command')
+        ])
+
+    def _graph_settings_group(self):
+        return dbc.FormGroup([
             dbc.Checklist(
-                options=[{"label": "Animate graph", "value": 'animate'}],
-                value=['animate'],
-                id="animate-switch",
-                switch=True),
+                    options=[{"label": "Animate graph", "value": 'animate'}],
+                    value=['animate'],
+                    id="animate-switch",
+                    switch=True),
             dbc.FormGroup(children=[
                 dbc.Label('Spacing between nodes: ', width=7),
                 dbc.Col(
@@ -32,12 +38,16 @@ class ConfigureTab(dbc.Tab):
                         type='number',
                         min=1,
                         value=2))],
-                row=True),
+                row=True)
+        ])
+
+    def _save_changes_group(self):
+        return dbc.FormGroup([
             dbc.Button('Save',
                 id='save-config-button',
                 color='primary',
                 className="mr-1"),
             Div(children=None,
                 id='save-config-notification',
-                style=button_style())],
-            style=tab_style())
+                style=button_style())
+        ])
