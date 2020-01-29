dd928508245eb198fa4f9a91d29d4501ad0e1fc0
MatyiDev <schubertmatyas@gmail.com>
2020-01-29 17:31:21 +0100

    Remove unnecessary ids from realtime tab

--- view/realtime_tab.py
+++ view/realtime_tab.py
@@ -25,66 +25,62 @@ class RealtimeTab(dbc.Tab):
             style=styles.tab_style())
 
     def _add_app_group(self):
-        return dbc.FormGroup(
-            id='add-app-groub',
-            children=[
-                dbc.Label('Add application to trace'),
-                dbc.Row([
-                    dbc.Col(dbc.Input(
-                        id='application-path',
-                        type='text',
-                        placeholder='/path/to/binary')),
-                    dbc.Col(dbc.Button('Add',
-                        id='add-app-button',
-                        color='primary',
-                        className='mr-1'),
-                        width=2)]),
-                dbc.FormText('Write path to runnable and click add'),
-                html.Div(
-                    id='add-app-notification',
-                    children=None,
-                    style=styles.button_style())])
+        return dbc.FormGroup([
+            dbc.Label('Add application to trace'),
+            dbc.Row([
+                dbc.Col(dbc.Input(
+                    id='application-path',
+                    type='text',
+                    placeholder='/path/to/binary')),
+                dbc.Col(dbc.Button('Add',
+                    id='add-app-button',
+                    color='primary',
+                    className='mr-1'),
+                    width=2)]),
+            dbc.FormText('Write path to runnable and click add'),
+            html.Div(
+                id='add-app-notification',
+                children=None,
+                style=styles.button_style())
+        ])
 
     def _manage_apps_group(self):
-        return dbc.FormGroup(
-            id='manage-apps-group',
-            children=[
-                dbc.Label('Manage applications'),
-                dbc.Select(
-                    id='applications-select',
-                    options=[]),
-                dbc.Button('Manage functions',
-                    id='manage-functions-button',
-                    color='success',
-                    className='mr-1',
-                    style=styles.button_style()),
-                dbc.Button('Remove application',
-                    id='remove-app-button',
-                    color='danger',
-                    className='mr-1',
-                    style=styles.button_style()),
-                html.Div(
-                    id='manage-apps-notification',
-                    children=None,
-                    style=styles.button_style())])
+        return dbc.FormGroup([
+            dbc.Label('Manage applications'),
+            dbc.Select(
+                id='applications-select',
+                options=[]),
+            dbc.Button('Manage functions',
+                id='manage-functions-button',
+                color='success',
+                className='mr-1',
+                style=styles.button_style()),
+            dbc.Button('Remove application',
+                id='remove-app-button',
+                color='danger',
+                className='mr-1',
+                style=styles.button_style()),
+            html.Div(
+                id='manage-apps-notification',
+                children=None,
+                style=styles.button_style())
+        ])
 
     def _trace_group(self):
-        return dbc.FormGroup(
-            id='trace-group',
-            children=[
-                daq.PowerButton(
-                    id='trace-button',
-                    on=False,
-                    color='#00FF00',
-                    style=styles.button_style()),
-                html.Div(
-                    id='trace-error-notification',
-                    children=None,
-                    style=styles.button_style())])
+        return dbc.FormGroup([
+            daq.PowerButton(
+                id='trace-button',
+                on=False,
+                color='#00FF00',
+                style=styles.button_style()),
+            html.Div(
+                id='trace-error-notification',
+                children=None,
+                style=styles.button_style())
+        ])
 
     def _config_path_group(self):
         return dbc.FormGroup(
-            id='config-path-group',
             children=[
                 dbc.Checklist(
                     options=self.config_path_swtich(),
@@ -96,7 +92,8 @@ class RealtimeTab(dbc.Tab):
                         id='config-file-path',
                         type='text',
                         placeholder='/path/to/config'),
-                    id="config-file-input-collapse")],
+                    id="config-file-input-collapse")
+            ],
             style=styles.button_style())
 
     def _timer(self):
