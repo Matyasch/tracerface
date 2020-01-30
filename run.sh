d99d92b14cda4ea31f7856fded19c2db64f06a8e
MatyiDev <schubertmatyas@gmail.com>
2020-01-30 19:48:27 +0100

    Add wrapper script for dependencies
    resolve #30

---
+++ run.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -e
+
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+function usage() {
+    echo "usage: $0 [-h --help] [-d --debug] [-l --routes-logging]"
+    echo
+    echo
+    echo "This script is a wrapper around main.py and its dependecnies"
+    echo
+    echo "Run this script if you want to start the application in a"
+    echo "virtual environment with all the required python package"
+    echo "dependencies preinstalled."
+    echo
+    echo "NOTE: Running this script for the first time may take a"
+    echo "considerable amount of time. If you have the needed python"
+    echo "packages installed already, feel free to run main.py directly"
+    echo
+    echo
+    echo "valid options:"
+    echo "      -h, --help              Show this message"
+    echo "      -d, --debug             Start server in debug mode"
+    echo "      -l, --routes-logging    Show routes access logging in the console"
+
+}
+
+
+start_command="python3 $ROOT_DIR/main.py"
+while getopts ":h" option;
+do
+    case $option in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -d|--debug)
+            start_command+=" --debug"
+            shift
+            ;;
+        -l|--routes-logging)
+            start_command+=" --routes-logging"
+            shift
+            ;;
+        \?)
+            echo "Error: Invalid option"
+            usage
+            exit 1;;
+    esac
+done
+
+
+python3 -m venv "$ROOT_DIR/bin-python"
+"$ROOT_DIR/bin-python/bin/pip" --disable-pip-version-check install  --no-cache-dir --requirement requirements.txt > bin-python/pip-install.log
+source "$ROOT_DIR/bin-python/bin/activate"
+echo $start_command
+$start_command
