7f8084a06cde365bd3ebbb2d3337f700d0fe12b8
Matyasch <schubertmatyas@gmail.com>
2020-06-29 19:01:24 +0200

    Remove unused bcc installed check
    It is done explicitly in run scripts

--- bcc-env.inc.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-# BCC package needs to be available as it can not be installed with pip
-[ -f "/usr/lib/python3/dist-packages/bcc/__init__.py" ] && {
-    export PYTHONPATH="$PYTHONPATH:/usr/lib/python3/dist-packages"
-} || {
-    echo "Please install the python3-bcc package"
-    exit 1
-}
