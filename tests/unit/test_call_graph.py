a896cd47cbbc459613763e1909d22363fa28b216
Matyasch <schubertmatyas@gmail.com>
2020-08-18 19:22:06 +0200

    Add missing shebangs to tests

--- tests/unit/test_call_graph.py
+++ tests/unit/test_call_graph.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from unittest import main, TestCase
 
 from tracerface.call_graph import CallGraph
--- tests/unit/test_trace_controller.py
+++ tests/unit/test_trace_controller.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from unittest import main, mock, TestCase
 
 from tracerface.trace_controller import TraceController
